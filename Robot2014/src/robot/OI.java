9d341abf1921146a3ea267480b5ce9227b4865d8
TrippW <trwlll@mindspring.com>
2014-01-22 17:52:04 -0600

    Init
    Initial code. Has very little

---
+++ Robot2014/src/robot/OI.java
@@ -0,0 +1,18 @@
+package robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+
+    public Joystick leftJoy = new Joystick(0), rightJoystick = new Joystick(1);
+
+    public OI() {
+        
+    }
+}
---
+++ Robot2014/src/robot/RobotMap.java
@@ -0,0 +1,22 @@
+package robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    public static final int
+            LEFT_MOTOR = 1,
+            RIGHT_MOTOR = 2,
+            
+            //Solenoids
+//            LEFT_SOLENOID = 1,
+            
+            //Compressor
+            COMPRESSOR_CHANNEL = 1,
+            
+            //Relay switch
+            RELAY_SWITCH_CHANNEL = 1;
+}
